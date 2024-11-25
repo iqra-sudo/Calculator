e770eac230e7b7b065a5749227c00671d371e40e
iqra-sudo <iqranadeem2006@gmail.com>
2024-11-25 21:41:47 +0500

    Update Program.cpp
    Added comments in code program.cpp

--- Program.cpp
+++ Program.cpp
@@ -1,1 +1,21 @@
-A C/C++ program to find the maximum of three numbers and then display it.
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a,b,c;
+  cout<<"Enter three numbers: ";
+  cin>>a>>b>>c;  //taking 3 numbers from user
+  if(a>b&&a>c)
+  {
+    cout<<a;
+  }
+  else if(b>a&&b>c)
+  {
+    cout<<b;
+  }
+  else
+  {
+    cout<<c;
+    /*If a nd b is smaller then c is greater*/ 
+  }
+}
